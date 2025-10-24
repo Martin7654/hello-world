d386180bd8f7e1d2995756b5c7d1b914c43e6904
Martin7654 <m.moser@edu.uni-graz.at>
2025-10-24 15:02:29 +0200

    jj

---
+++ 3. lesson/range.py
@@ -0,0 +1,12 @@
+total = 0
+for i in range(5):  # 0 is first number
+    total += i      #adds 0 + 1 + 2 + 3 + 4, cumulative sum
+    print(total)
+
+for _i in range(5): #ignore variable with the _, but not get an error
+    print("Repeated action")
+
+sum_even = 0
+for i in range(10, 0, -2):
+    sum_even += i
+    print(sum_even)
